b307d5aaf3534ae1eed0c8c816b25cbfa117d068
ulmu0426 <80729771+ulmu0426@users.noreply.github.com>
2023-08-11 19:37:20 +0900

    Card 엔티티 모델 생성

---
+++ src/main/java/com/ulmu/loacardset_web/model/Card.java
@@ -0,0 +1,41 @@
+package com.ulmu.loacardset_web.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "card", schema = "loa_card", uniqueConstraints = {@UniqueConstraint(columnNames = "card_id")})
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "card_id", unique = true)
+    private Long cardId;        //카드 pk
+
+    @Column(name = "name", unique = false)
+    private String name;        //카드이름
+
+    @Column(name = "count", unique = false)
+    private int count;          //보유
+
+    @Column(name = "awake", unique = false)
+    private int awake;          //각성수치
+    
+    @Column(name = "grade", unique = false)
+    private String grade;       //카드 등급
+
+    @Column(name = "getCard", unique = false)
+    private boolean getCard;    //카드 획득 유무
+
+    @Column(name = "path", unique = false)
+    private String path;        //카드 이미지 경로
+
+    public Card() {     //보유량, 각성도, 획득유무 기본값 없음
+        this.count = 0;
+        this.awake = 0;
+        this.getCard = false;
+    }
+}

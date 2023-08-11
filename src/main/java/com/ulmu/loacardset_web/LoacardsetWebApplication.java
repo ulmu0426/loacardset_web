ea392b512befb362e9ef74b3fee1de7c7173b06f
ulmu0426 <80729771+ulmu0426@users.noreply.github.com>
2023-08-11 19:24:38 +0900

    Initial commit

---
+++ src/main/java/com/ulmu/loacardset_web/LoacardsetWebApplication.java
@@ -0,0 +1,13 @@
+package com.ulmu.loacardset_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoacardsetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoacardsetWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ulmu/loacardset_web/LoacardsetWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulmu.loacardset_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoacardsetWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

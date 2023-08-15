360fb5e354621e00609d6f52c3c0b89db7cb3e5a
ulmu0426 <80729771+ulmu0426@users.noreply.github.com>
2023-08-15 23:04:53 +0900

    CardMapper getList 대응 메소드 작성
    List<> 형식 Get 요청에 응답 할 수 있도록 convertToCardDtoList, convertToCardModelList 작성

--- src/main/java/com/ulmu/loacardset_web/mapper/CardMapper.java
+++ src/main/java/com/ulmu/loacardset_web/mapper/CardMapper.java
@@ -3,6 +3,12 @@ package com.ulmu.loacardset_web.mapper;
 import com.ulmu.loacardset_web.dto.CardDto;
 import com.ulmu.loacardset_web.model.Card;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Collectors;
+
 public class CardMapper {
     public static CardDto convertToCardDto(Card card){
         CardDto cardDto = new CardDto();
@@ -28,5 +34,17 @@ public class CardMapper {
         card.setPath(cardDto.getPath());
 
         return card;
+    }
+
+    public static List<CardDto> convertToCardDtoList(List<Card> cards) {
+        return cards.stream()
+                .map(CardMapper::convertToCardDto)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Card> convertToCardModelList(List<CardDto> cardDtos) {
+        return cardDtos.stream()
+                .map(CardMapper::convertToCardModel)
+                .collect(Collectors.toList());
     }
 }
